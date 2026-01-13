b1e9dbe958b301e6360161ecee48c091e538f707
macrocane <direnzomanuel@gmail.com>
2026-01-13 21:07:01 +0100

    allineato id progetto

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -6,13 +6,12 @@ import { initializeApp } from "firebase/app";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 export const firebaseConfig = {
-  apiKey: "AIzaSyC8CqWZik-8kdRFu0wzo18hs0Sf2fAOZXU",
-  authDomain: "macrocanequiz.firebaseapp.com",
-  projectId: "macrocanequiz",
-  storageBucket: "macrocanequiz.firebasestorage.app",
-  messagingSenderId: "1052480955138",
-  appId: "1:1052480955138:web:f5ec8d5ce496972949981b",
-  measurementId: "G-32KKFDQDLM"
+  projectId: "studio-5762279613-4541b",
+  appId: "1:1006064055262:web:9863a3c01db7b755b3a34f",
+  apiKey: "AIzaSyCWwtMtMkpegSgZs89ENEjSziNEP1BDwGo",
+  authDomain: "studio-5762279613-4541b.firebaseapp.com",
+  messagingSenderId: "1006064055262",
+  measurementId: "",
 };
 
 // Initialize Firebase
